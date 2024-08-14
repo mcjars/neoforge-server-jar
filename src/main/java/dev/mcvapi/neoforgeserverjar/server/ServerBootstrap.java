fb28006356d4bf7d91bb43419f3ddc4c8f89a811
0x7d8 <rjansengd@gmail.com>
2024-08-15 01:08:48 +0200

    better ctrl c handling (?)

--- src/main/java/dev/mcvapi/neoforgeserverjar/server/ServerBootstrap.java
+++ src/main/java/dev/mcvapi/neoforgeserverjar/server/ServerBootstrap.java
@@ -1,33 +1,60 @@
 package dev.mcvapi.neoforgeserverjar.server;
 
-import java.io.IOException;
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.ptr.IntByReference;
 
 public class ServerBootstrap {
-	public void startServer(String[] cmd) throws ServerStartupException {
-		try {
-			Process process = new ProcessBuilder(cmd)
-				.command(cmd)
-				.inheritIO()
-				.start();
+    public interface CLibrary extends Library {
+			CLibrary INSTANCE = Native.load("c", CLibrary.class);
 
-			// Forward Ctrl+C signal to the child process
+			int kill(int pid, int sig);
+    }
+
+    public static final int SIGINT = 2;
+    public static final int SIGTERM = 15;
+
+	public void startServer(String[] cmd) throws ServerStartupException {
+		Process process = null;
+		try {
+			ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+			processBuilder.inheritIO();
+			process = processBuilder.start();
+
 			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-				process.destroy();
-				try {
-					process.waitFor();
-				} catch (InterruptedException e) {
+				if (process.isAlive()) {
+					try {
+						forwardSignal(process.pid(), SIGINT);
+						process.waitFor();
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+					}
 				}
 			}));
 
-			while (process.isAlive()) {
+			while (true) {
 				try {
 					process.waitFor();
 					break;
-				} catch (InterruptedException ignore) {
+				} catch (InterruptedException e) {
+					Thread.currentThread().interrupt();
 				}
 			}
 		} catch (IOException exception) {
 			throw new ServerStartupException("Failed to start the NeoForge server.", exception);
+		} finally {
+			if (process != null && process.isAlive()) {
+				process.destroy();
+			}
+		}
+	}
+
+	private void forwardSignal(long pid, int signal) {
+		int pidInt = (int) pid;
+		int result = CLibrary.INSTANCE.kill(pidInt, signal);
+		if (result != 0) {
+			System.err.println("Failed to send signal " + signal + " to process " + pid + ". Error code: " + result);
 		}
 	}
 
